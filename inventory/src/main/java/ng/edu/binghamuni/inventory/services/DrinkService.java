ca6aa3bd14bc72f38ea745ebcca6f54e4aa0c8ed
majorwanda <133369217+majorwanda@users.noreply.github.com>
2023-12-07 09:45:11 +0100

    Update DrinkService.java

--- inventory/src/main/java/ng/edu/binghamuni/inventory/services/DrinkService.java
+++ inventory/src/main/java/ng/edu/binghamuni/inventory/services/DrinkService.java
@@ -1,1 +1,15 @@
+package ng.edu.binghamuni.inventory.services;
+
+import ng.edu.binghamuni.inventory.domain.Drink;
+
+import java.util.List;
+
+public interface DrinkService
+{
+    Drink saveDrink(Drink drink);
+    Drink getDrinkById(long id);
+    List<Drink> getAllDrinks();
+    Drink updateDrink(Drink drink);
+    void deleteDrink(long id);
+}
 
